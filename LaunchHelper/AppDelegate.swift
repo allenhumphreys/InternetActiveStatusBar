1a96eaee62cdeb7bcc8f6e4a24d5f47aec3cf1e5
Allen Humphreys <allen.humphreys@icloud.com>
2019-05-14 19:12:28 -0500

    Add launch helper target and embed it in primary app

---
+++ LaunchHelper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LaunchHelper
+//
+//  Created by Allen Humphreys on 5/14/19.
+//  Copyright © 2019 Demo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
