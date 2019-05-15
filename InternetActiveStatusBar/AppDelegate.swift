6813ac059560fa66ade40c2ba315a8fdb69931ab
Allen Humphreys <allen.humphreys@icloud.com>
2019-05-14 19:08:54 -0500

    Start application from Xcode template

---
+++ InternetActiveStatusBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  InternetActiveStatusBar
+//
+//  Created by Allen Humphreys on 5/14/19.
+//  Copyright © 2019 Demo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
